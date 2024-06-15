a81fcffd8815604224d7e4c6b514c20291e1a94a
José Anthony Arenas Conde <u20211d744@upc.edu.pe>
2024-06-14 22:08:48 -0500

    prueba

--- src/main/java/pe/edu/upc/gift_service/util/CorsConfig.java
+++ src/main/java/pe/edu/upc/gift_service/util/CorsConfig.java
@@ -6,12 +6,12 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200", "https://dashing-sfogliatella-a0e9f9.netlify.app", "https://gift-service-d044a.web.app")
+                .allowedOrigins("https://gift-service-d044a.web.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
